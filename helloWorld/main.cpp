903f31479b9b806e672f5392f4693a1cb66e9782
Miguel Rust <hava12soft@gmail.com>
2019-06-03 20:53:48 -0600

    Initial Commit

---
+++ helloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Admin on 6/3/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
