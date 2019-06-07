692db06685f7bb1005c9dc6cece27240c29d020c
Miguel Rust <hava12soft@gmail.com>
2019-06-06 19:08:35 -0600

    Created C++ Monty Hall Problem Simulator

--- helloWorld/main.cpp
+++ helloWorld/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+bool simulateDoorPick(bool willSwitch){
+    const int doorChosen = rand() % 3;
+    const int winningDoor = rand() % 3;
+    if(willSwitch){
+        return doorChosen != winningDoor;
+    }else{
+        return doorChosen == winningDoor;
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int sampleSize;
+    string phrase = "What is your prefered sample size?" ;
+    cout << phrase << endl;
+    cin >> sampleSize;
+    int switchWins = 0;
+    int noSwitchWins = 0;
+    for(int i = 0; i < sampleSize; i++){
+        if(simulateDoorPick(true)) switchWins++;
+        if(simulateDoorPick(false)) noSwitchWins++;
+    }
+    const double switchProportion = (double)switchWins/(double)sampleSize;
+    const double noSwitchProportion = (double)noSwitchWins/(double)sampleSize;
+    cout << "Switch Stats" << endl << "Proportion: " << switchProportion << endl << "No Switch Stats" << endl << "Proportion: " << noSwitchProportion << endl;
     return 0;
 }
+
+
+
